fce0fd344b38ce492d44d7b8539517e59d3cdfdd
Shahroz Afzal <your-email@gmail.com>
2026-09-13 19:44:46 +0500

    Add backend and organize frontend

---
+++ backend/index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const UserModel = require('./models/Users')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb://localhost:27017/crud").then(()=>{
+    console.log("DB is connected")
+}).catch((err)=>{
+console.log("err while connecting Db with Error", err)
+})
+
+app.get('/',(req,res) =>{
+    UserModel.find({})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.get('/getUser/:id',(req,res) => {
+    const id = req.params.id;
+    UserModel.findById({_id:id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+
+
+app.put('/updateUser/:id',(req,res) =>{
+    const id = req.params.id;
+UserModel.findByIdAndUpdate({_id:id},{
+    name:req.body.name,
+    email:req.body.email,
+    age:req.body.age})
+.then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+// cloudinary + multer
+app.delete('/deleteUser/:id',(req, res) =>{
+    const id = req.params.id;
+UserModel.findByIdAndDelete({_id: id})
+.then(res => res.json(res))
+.catch(err => res.json(err))
+})
+
+
+app.post("/createUser",(req,res) =>{
+    console.log(req.body)
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.listen(3001, () =>{
+    console.log("server is Running")
+})
+
